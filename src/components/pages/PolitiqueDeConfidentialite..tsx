04b37750b97625d6e5e01671657ccbc6fdf4494d
johan ARREDONDO <johanarredondo31@gmail.com>
2024-09-03 11:52:50 +0200

    correction politique confidentialité

--- src/components/pages/PolitiqueDeConfidentialite..tsx
+++ src/components/pages/PolitiqueDeConfidentialite..tsx
@@ -1,16 +1,49 @@
-import { Heading } from "react-bulma-components";
+const PrivacyPolicy = () => {
+  return (
+    <div>
+      <header>
+        
+      </header>
 
-function PolitiqueDeConfidentialite() {
+      <main className="section">
+        <div className="container">
 
+          <section className="box">
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            </p>
+          </section>
 
-    return (
-       <main>
-        <div>
-        <Heading>Politique de confidentialité</Heading>
+          <section className="box">
+            <h3 className="title is-4">LES TYPES DE DONNÉES TRAITÉES</h3>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+          </section>
+
+          <section className="box">
+            <h3 className="title is-4">LES TRAITEMENTS DES DONNÉES</h3>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+          </section>
+
+          <section className="box">
+            <h3 className="title is-4">BASE JURIDIQUE DU TRAITEMENT</h3>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+          </section>
+
+        </div>
+      </main>
+
+      <footer>
+        
+      </footer>
     </div>
-    
-        </main>
-    )
-  }
-  
-  export default PolitiqueDeConfidentialite;
+  );
+};
+
+export default PrivacyPolicy;
+
